3b4beab3b227fe7e1f2acaa7e09a803c8d04e964
unknown <telegaru_andrei@yahoo.com>
2023-10-19 21:13:14 +0300

    numereimpare

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,35 @@ import Task1.Operatii;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String operatie = scanner.next().toUpperCase();
 
         Calculator calculator = new Calculator(5, 10);
-        calculator.setTermenUnu(2.3f);
-        calculator.setTermenDoi(5f);
+        calculator.setTermenUnu(2);
+        calculator.setTermenDoi(5);
 
-        if (Operatii.ADUNARE.toString().equals(operatie)){
-            System.out.println(calculator.adunare());
-        } else if (Operatii.SCADERE.toString().equals(operatie)) {
-            System.out.println(calculator.scadere());
-        } else {
-            System.out.println("Valoarea introdusa invalida");
-        }
 
+//        String operatie = scanner.next().toUpperCase();
+//        if (Operatii.ADUNARE.toString().equals(operatie)){
+//            System.out.println(calculator.adunare());
+//        } else if (Operatii.SCADERE.toString().equals(operatie)) {
+//            System.out.println(calculator.scadere());
+//        } else {
+//            System.out.println("Valoarea introdusa invalida");
+//        }
         //        System.out.println(calculator.adunare());
-//        System.out.println(calculator.scadere());
+        //        System.out.println(calculator.scadere());
+//        System.out.println("Introduceti codul operatiei dorite: 1 sau 2");
+//        int cod = scanner.nextInt();
+//        if (Operatii.ADUNARE.getCod() == cod) {
+//            System.out.println(calculator.adunare());
+//        } else if (Operatii.SCADERE.getCod() == cod) {
+//            System.out.println(calculator.scadere());
+//        } else {
+//            System.out.println("Valoarea nu este valida");
+//        }
+        System.out.println(calculator.numereImpare());
     }
+
 }
--- src/Task1/Operatii.java
+++ src/Task1/Operatii.java
@@ -1,7 +1,21 @@
 package Task1;
 
 public enum Operatii {
-    ADUNARE,
-    SCADERE;
+    ADUNARE (1, "adunare", "Operatii adunare"),
+    SCADERE (2, "scadere", "Operatii scadere");
 
+    private final int cod;
+    private final String valoare;
+    private final String nume;
+
+
+    Operatii(int cod, String valoare, String nume){
+        this.cod = cod;
+        this.valoare = valoare;
+        this.nume = nume;
+    }
+
+    public int getCod() {
+        return cod;
+    }
 }
