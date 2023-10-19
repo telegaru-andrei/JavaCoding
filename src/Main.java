ff48d14da29a88a52ea32e7ab37e02fc4444acae
unknown <telegaru_andrei@yahoo.com>
2023-10-19 19:21:51 +0300

    Metoda adunare.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import Task1.Calculator;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        Calculator calculator = new Calculator(5, 10);
+        calculator.setTermenUnu(2.3f);
+        calculator.setTermenDoi(5f);
+
+        System.out.println(calculator.adunare());
     }
 }
